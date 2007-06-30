7262d090a82f070dc0ac99d50136e9e29d73b1e1
perldomm <perldomm@1f13776b-372c-0410-b9e8-ed37f72ba318>
2007-06-30 20:22:11 +0000

    r6577@i85:  domm | 2007-06-30 21:18:43 +0200  added --yaml to cpants_lint.pl

--- Module-CPANTS-Analyse/bin/cpants_lint.pl
+++ Module-CPANTS-Analyse/bin/cpants_lint.pl
@@ -5,9 +5,10 @@ use Module::CPANTS::Analyse;
 use Getopt::Long;
 use IO::Capture::Stdout;
 use Data::Dumper;
+use YAML;
 
 my %opts;
-GetOptions(\%opts,qw(dump! no_capture! verbose!));
+GetOptions(\%opts,qw(dump no_capture! verbose! yaml to_file dir=s ));
 
 my $dist=shift(@ARGV);
 
@@ -18,71 +19,84 @@ my $mca=Module::CPANTS::Analyse->new({
     dist=>$dist,
     opts=>\%opts,
 });
+
+my $output;
+
 my $cannot_unpack=$mca->unpack;
 if ($cannot_unpack) {
     if ($opts{dump}) {
-        print Dumper($mca->d);
+        $output=Dumper($mca->d);
+    } elsif ($opts{yaml}) {
+        $output=Dump($mca->d);
     } else {
-        print "Cannot unpack \t\t".$mca->tarball,"\n";
+        $output="Cannot unpack \t\t".$mca->tarball."\n";
     }
-    exit;
-}
+} 
+else {
+    $mca->analyse;
+    $mca->calc_kwalitee;
 
-$mca->analyse;
-$mca->calc_kwalitee;
-
-
-if ($opts{dump}) {
-    $Data::Dumper::Sortkeys=1;
-    print Dumper($mca->d);
-} else {
-
-    # build up lists of failed metrics
-    my (@core_failure,@opt_failure);
-    my ($core_kw,$opt_kw)=(0,0);
-    my $kwl=$mca->d->{kwalitee};
+    if ($opts{dump}) {
+        $Data::Dumper::Sortkeys=1;
+        $output=Dumper($mca->d);
+    } elsif ($opts{yaml}) {
+        $output=Dump($mca->d);
+    } else {
+    
+        # build up lists of failed metrics
+        my (@core_failure,@opt_failure);
+        my ($core_kw,$opt_kw)=(0,0);
+        my $kwl=$mca->d->{kwalitee};
         
-    foreach my $ind (@{$mca->mck->get_indicators}) {
-        if ($ind->{is_extra}) {
-            next if $ind->{name} eq 'is_prereq';
-            if ($kwl->{$ind->{name}}) {
-                $opt_kw++;
+        foreach my $ind (@{$mca->mck->get_indicators}) {
+            if ($ind->{is_extra}) {
+                next if $ind->{name} eq 'is_prereq';
+                if ($kwl->{$ind->{name}}) {
+                    $opt_kw++;
+                } else {
+                    push(@opt_failure,"* ".$ind->{name}."\n".$ind->{remedy});
+                }
             } else {
-                push(@opt_failure,"* ".$ind->{name}."\n".$ind->{remedy});
+                if ($kwl->{$ind->{name}}) {
+                    $core_kw++;
+                } else {
+                    push(@core_failure,"* ".$ind->{name}."\n".$ind->{remedy});
+                }
             }
+        }
+
+        # output results 
+        $output.="Checked dist \t\t".$mca->tarball."\n";
+
+        my $max_core_kw=$mca->mck->available_kwalitee;
+        my $max_kw=$mca->mck->total_kwalitee;
+        my $total_kw=$core_kw+$opt_kw;
+
+        $output.="Kwalitee rating\t\t".sprintf("%.2f",100*$total_kw/$max_core_kw)."% ($total_kw/$max_core_kw)\n";
+
+
+        if ($total_kw == $max_kw -1) {  # -1 because of is_prereq
+            $output.="\nCongratulations for building a 'perfect' distribution!\n";
         } else {
-            if ($kwl->{$ind->{name}}) {
-                $core_kw++;
-            } else {
-                push(@core_failure,"* ".$ind->{name}."\n".$ind->{remedy});
+            if (@core_failure) {
+                $output.="\nHere is a list of failed Kwalitee tests and\nwhat you can do to solve them:\n\n";
+                $output.=join ("\n\n",@core_failure,'');
             }
-        }
-    }
-
-    # output results 
-    print "\n";
-    print "Checked dist \t\t".$mca->tarball,"\n";
-
-    my $max_core_kw=$mca->mck->available_kwalitee;
-    my $max_kw=$mca->mck->total_kwalitee;
-    my $total_kw=$core_kw+$opt_kw;
-
-    print "Kwalitee rating\t\t".sprintf("%.2f",100*$total_kw/$max_core_kw)."% ($total_kw/$max_core_kw)\n";
-
-
-    if ($total_kw == $max_kw -1) {  # -1 because of is_prereq
-        print "\nCongratulations for building a 'perfect' distribution!\n";
-    } else {
-        if (@core_failure) {
-            print "\nHere is a list of failed Kwalitee tests and\nwhat you can do to solve them:\n\n";
-            print join ("\n\n",@core_failure,'');
-        }
-        if (@opt_failure) {
-            print "\nFailed optional Kwalitee tests and\nwhat you can do to solve them:\n\n";
-            print join ("\n\n",@opt_failure,'');
+            if (@opt_failure) {
+                $output.="\nFailed optional Kwalitee tests and\nwhat you can do to solve them:\n\n";
+                $output.=join ("\n\n",@opt_failure,'');
+            }
         }
     }
 }
+
+if ($opts{to_file}) {
+
+} else {
+    print $output;
+}
+
+
 __END__
 
 =head1 NAME
