9f70ea0f9da66dfca6ce9eed2b545840b3642e14
perldomm <perldomm@1f13776b-372c-0410-b9e8-ed37f72ba318>
2007-08-19 14:19:13 +0000

    r6844@i85:  domm | 2007-08-19 16:19:18 +0200  fixed some local paths

--- Module-CPANTS-ProcessCPAN/bin/run.pl
+++ Module-CPANTS-ProcessCPAN/bin/run.pl
@@ -12,12 +12,13 @@ my $perl=$^X;
 my $hostname=hostname();
 my $path=$FindBin::Bin;
 my $lib=$path."/../lib";
-my ($cpan,$lint,$site);
+my ($cpan,$lint,$site,$yaml);
 
 if ($hostname =~/hexten/) {
     $cpan='/home2/no-backup/CPAN/cpan.hexten.net/htdocs/';
     $lint='/home/domm/Module-CPANTS-Analyse/bin/cpants_lint.pl';
     $site='/home/domm/Module-CPANTS-Site/';
+    $yaml='/home/domm/Module-CPANTS-ProcessCPAN/yaml/';
 
     $ENV{CPANTS_USER}='cpants';
     unless ($ENV{CPANTS_PWD}) {
@@ -30,12 +31,16 @@ if ($hostname =~/hexten/) {
     
 } else {
     $cpan='/home/minicpan';
-    $lint='/home/domm/perl/Module-CPANTS-Analyse/bin/cpants_lint.pl';
-    $site='/home/domm/perl/Module-CPANTS-Site/';
+    $lint='/home/domm/perl/cpants/Module-CPANTS-Analyse/bin/cpants_lint.pl';
+    $site='/home/domm/perl/cpants/Module-CPANTS-Site/';
+    $yaml='/home/domm/perl/cpants/Module-CPANTS-ProcessCPAN/yaml/';
 }
 
-#system("$perl -I$lib $path/analyse_cpan.pl --cpan $cpan --lint $lint 
-#$force");
+my $yaml_in=$yaml.'in/';
+my $yaml_out=$yaml.'out/';
+system("$perl -I$lib $path/analyse_cpan.pl --cpan $cpan --lint $lint --dir $yaml_in $force");
+# TODO
+# take each yaml file, stuff it in DB, move it to $yaml_out
 system($perl,"-I$lib", $path."/run_complex_db_stuff.pl",$cpan);
 system($perl,"-I$lib", $path."/update_authors.pl",$cpan);
 system($perl,"-I$lib", $path."/make_graphs.pl",$site."root/static/");
