edb562d0a8f5b6dfb7e9e7d262fb2d014531a574
perldomm <perldomm@1f13776b-372c-0410-b9e8-ed37f72ba318>
2008-04-07 23:29:45 +0000

    r472@d610:  domm | 2008-04-08 01:21:12 +0200  dist housekeeping]

--- Module-CPANTS-ProcessCPAN/lib/Module/CPANTS/ProcessCPAN.pm
+++ Module-CPANTS-ProcessCPAN/lib/Module/CPANTS/ProcessCPAN.pm
@@ -15,7 +15,7 @@ use FindBin;
 use File::Copy;
 use DateTime;
 
-use version; our $VERSION=version->new('0.74');
+use version; our $VERSION=version->new('0.75');
 
 __PACKAGE__->mk_accessors(qw(cpan lint force run prev_run _db _db_hist mck));
 
