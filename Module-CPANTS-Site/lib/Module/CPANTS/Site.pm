be8355e14b75fab16f9bc47ed7ca34ba913f4b5b
perldomm <perldomm@1f13776b-372c-0410-b9e8-ed37f72ba318>
2008-08-10 11:20:53 +0000

    r700@d610:  domm | 2008-08-10 13:15:53 +0200  bumped version to 0.76

--- Module-CPANTS-Site/lib/Module/CPANTS/Site.pm
+++ Module-CPANTS-Site/lib/Module/CPANTS/Site.pm
@@ -14,7 +14,7 @@ my $home=Module::CPANTS::ProcessCPAN::ConfigData->config('home');
 my $db_user=Module::CPANTS::ProcessCPAN::ConfigData->config('db_user');
 my $db_pwd=Module::CPANTS::ProcessCPAN::ConfigData->config('db_pwd');
 $Template::Directive::WHILE_MAX=3000;
-use version; our $VERSION = version->new('0.75');
+use version; our $VERSION = version->new('0.76');
 
 
 __PACKAGE__->config(
