439b23c39ecbb15f5b12d313714b16d28c9ff252
perldomm <perldomm@1f13776b-372c-0410-b9e8-ed37f72ba318>
2009-04-10 19:09:12 +0000

    its called FastMmap...;

--- Module-CPANTS-Site/lib/Module/CPANTS/Site.pm
+++ Module-CPANTS-Site/lib/Module/CPANTS/Site.pm
@@ -6,7 +6,7 @@ use Module::CPANTS::ProcessCPAN::ConfigData;
 use File::Spec::Functions;
 use Catalyst qw(Static::Simple
     Session
-    Session::Store::FastMMap
+    Session::Store::FastMmap
     Session::State::Cookie
 );
 
